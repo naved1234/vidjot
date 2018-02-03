2e2cd66f90b3ada3a74d9d9f2ea540ad02c2204b
Naved <navedahmed@urbanclap.com>
2018-02-03 19:48:39 +0530

    added required validation both on client/server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -21,7 +22,9 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');   
 
-const port = 5000;
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 //Index Route
 app.get('/', (req, res) => {
@@ -40,6 +43,29 @@ app.get('/about', (req, res) => {
 app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
 });
+
+//Process Form
+app.post('/ideas', (req, res) => {
+    let errors = [];
+
+    if(!req.body.title) {
+        errors.push({text: 'Please add a title'});
+    }
+    if(!req.body.details) {
+        errors.push({text: 'Please add some details'});
+    }
+    if(errors.length > 0) {
+        res.render('ideas/add', {
+          errors: errors,
+          title: req.body.title,
+          details: req.body.details
+        });
+    } else {
+        res.send('passed');
+    }
+});
+
+const port = 5000;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
