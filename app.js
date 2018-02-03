d531685e46f206e5aa6df4504ead19d39996f77f
Naved <navedahmed@urbanclap.com>
2018-02-03 18:07:55 +0530

    some initial changes

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');   
+
+const port = 5000;
+
+//Index Route
+app.get('/', (req, res) => {
+    const title = 'Welcome';
+    res.render('index', {
+        title: title
+    });
+});
+
+//About Route
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
