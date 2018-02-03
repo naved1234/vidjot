596665da176fbb95e69f50c772645797430929b7
Naved <navedahmed@urbanclap.com>
2018-02-03 18:39:38 +0530

    added mongoose

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//Map global promise - get rid of warning
+mongoose.Promise = global.Promise;  
+//connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev')
+    .then(() => console.log('MongoDB connected.....'))
+    .catch(err => console.log(err));
+
+//Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
