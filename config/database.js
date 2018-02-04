bf2d5a1e38c78258de5f87905cb21f84dcde26d8
Naved <navedahmed@urbanclap.com>
2018-02-05 02:34:32 +0530

    removed === to == in config/database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV == 'production') {
     module.exports = {
         mongoURI: 'mongodb://naved1234:n@ved786@ds225038.mlab.com:25038/vidjot-live'
     };
